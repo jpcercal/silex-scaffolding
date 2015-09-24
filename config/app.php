f3a2b76e553c036ab43af997674d76835ec20e98
unknown <jcercal@DT04152.mercador.local>
2015-09-24 10:45:32 -0300

    Added the WebProfiler.

--- config/app.php
+++ config/app.php
@@ -6,8 +6,11 @@ use App\ServiceProvider\DoctrineExtensionsServiceProvider;
 use App\ServiceProvider\TranslationLoaderFilesServiceProvider;
 use Dflydev\Silex\Provider\DoctrineOrm\DoctrineOrmServiceProvider;
 use JDesrosiers\Silex\Provider\CorsServiceProvider;
+use Saxulum\DoctrineOrmManagerRegistry\Silex\Provider\DoctrineOrmManagerRegistryProvider;
+use Saxulum\SaxulumWebProfiler\Provider\SaxulumWebProfilerProvider;
 use Silex\Application;
 use Silex\Provider\DoctrineServiceProvider;
+use Silex\Provider\HttpFragmentServiceProvider;
 use Silex\Provider\MonologServiceProvider;
 use Silex\Provider\ServiceControllerServiceProvider;
 use Silex\Provider\SessionServiceProvider;
@@ -15,6 +18,7 @@ use Silex\Provider\SwiftmailerServiceProvider;
 use Silex\Provider\TranslationServiceProvider;
 use Silex\Provider\TwigServiceProvider;
 use Silex\Provider\UrlGeneratorServiceProvider;
+use Silex\Provider\WebProfilerServiceProvider;
 use Symfony\Component\HttpFoundation\Request;
 
 $app = new Application();
@@ -29,6 +33,8 @@ $app->register(new DoctrineServiceProvider(),    $doctrine['dbal']);
 $app->register(new DoctrineOrmServiceProvider(), $doctrine['orm']);
 
 $app->register(new DoctrineExtensionsServiceProvider());
+$app->register(new HttpFragmentServiceProvider());
+$app->register(new SessionServiceProvider());
 $app->register(new SessionServiceProvider());
 $app->register(new UrlGeneratorServiceProvider());
 $app->register(new ServiceControllerServiceProvider());
@@ -39,6 +45,12 @@ $app->register(new MonologServiceProvider(),     require CONFIG_PATH . DIRECTORY
 $app->register(new SwiftmailerServiceProvider(), require CONFIG_PATH . DIRECTORY_SEPARATOR . 'swiftmailer.php');
 $app->register(new TranslationServiceProvider(), require CONFIG_PATH . DIRECTORY_SEPARATOR . 'translation.php');
 $app->register(new TwigServiceProvider(),        require CONFIG_PATH . DIRECTORY_SEPARATOR . 'twig.php');
+
+if ($app['debug']) {
+    $app->register(new WebProfilerServiceProvider(), require CONFIG_PATH . DIRECTORY_SEPARATOR . 'webprofiler.php');
+    $app->register(new DoctrineOrmManagerRegistryProvider());
+    $app->register(new SaxulumWebProfilerProvider());
+}
 
 require CONFIG_PATH . DIRECTORY_SEPARATOR . 'error.php';
 
---
+++ config/webprofiler.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+    'profiler.cache_dir'    => STORAGE_PATH_CACHE . DIRECTORY_SEPARATOR . 'web.profiler',
+    'profiler.mount_prefix' => '/_profiler',
+];
