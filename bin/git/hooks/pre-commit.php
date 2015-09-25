6c1f979abff48df57f37483f12581dd9b1bdb1ac
João Paulo Cercal <jpcercal@gmail.com>
2015-09-25 11:03:51 -0300

    Added the StaticReview library.

--- bin/git/hooks/pre-commit.php
+++ bin/git/hooks/pre-commit.php
@@ -4,7 +4,6 @@
 $autoload = realpath(__DIR__ . '/../../../vendor/autoload.php');
 
 if (!file_exists($autoload)) {
-
     $message = <<<EOT
 You must set up the project dependencies, run the following commands:\n
 curl -sS https://getcomposer.org/installer | php\n
@@ -12,6 +11,7 @@ php composer.phar install\n
 EOT;
 
     echo str_replace('\\n', PHP_EOL, $message);
+
     exit(1);
 }
 
@@ -24,7 +24,6 @@ use StaticReview\Review\Composer\ComposerLintReview;
 use StaticReview\Review\Composer\ComposerSecurityReview;
 use StaticReview\Review\General\LineEndingsReview;
 use StaticReview\Review\PHP\PhpCodeSnifferReview;
-use StaticReview\Review\PHP\PhpLeadingLineReview;
 use StaticReview\Review\PHP\PhpLintReview;
 use StaticReview\StaticReview;
 use StaticReview\VersionControl\GitVersionControl;
@@ -35,33 +34,52 @@ $git      = new GitVersionControl();
 $review   = new StaticReview($reporter);
 
 // Add any reviews to the StaticReview instance, supports a fluent interface.
-$review->addReview(new LineEndingsReview())
-       ->addReview(new PhpLeadingLineReview())
-       ->addReview(new PhpLintReview())
-       ->addReview(new ComposerLintReview())
-       ->addReview(new ComposerSecurityReview());
+$review
+    ->addReview(new LineEndingsReview())
+    ->addReview(new PhpLintReview())
+    ->addReview(new ComposerLintReview())
+    ->addReview(new ComposerSecurityReview())
+;
 
 $codeSniffer = new PhpCodeSnifferReview();
 $codeSniffer->setOption('standard', 'PSR2');
 $review->addReview($codeSniffer);
+
+if ($git->getStagedFiles()->count() === 0) {
+    $climate
+        ->out('')
+        ->yellow('[-] Nothing to do.')
+        ->white('No files founded in the git staged.')
+    ;
+
+    exit(0);
+}
 
 // Review the staged files.
 $review->files($git->getStagedFiles());
 
 // Check if any matching issues were found.
 if ($reporter->hasIssues()) {
-    $climate->out('')->out('');
+    $climate
+        ->out('')
+        ->out('')
+    ;
 
     foreach ($reporter->getIssues() as $issue) {
         $climate->red($issue);
     }
 
-    $climate->out('')->red('[x] Please fix the errors above.');
-
+    $climate
+        ->out('')
+        ->red('[x] Please fix the errors above.')
+    ;
     exit(1);
 
 } else {
-    $climate->out('')->green('[+] Very well guy.')->white('No issues detected, the commit will be performed in the next step.');
-
+    $climate
+        ->out('')
+        ->green('[+] Very well guy.')
+        ->white('No issues detected, the commit will be performed in the next step.')
+    ;
     exit(0);
 }
